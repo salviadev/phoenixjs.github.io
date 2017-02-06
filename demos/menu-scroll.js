444d054af32e122bc8cb2dc5e53c2273a35cab28
idsok <i.sokhona@salviadeveloppement.com>
2017-02-06 14:32:11 +0100

    add menu-scroll demo

---
+++ demos/menu-scroll.js
@@ -0,0 +1,175 @@
+/* global $ */
+$(function () {
+
+    var layout = {
+        "name": "form",
+        "$type": "block",
+        "$items": [{
+            "$type": "row",
+            "$forceTable": true,
+            "$items": [
+                {
+                    $customColSize: 'demo-100',
+                    "$items": [
+                        {
+                            "$type": "block",
+                            "$name": "one",
+                            "$title": {
+                                "value": "One"
+                            },
+
+                            "$items": [
+                                {
+                                    "$type": "html",
+                                    "$html": "<div>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec mi a sem fermentum iaculis sit amet eget odio. Duis consectetur sollicitudin arcu eu convallis. In hac habitasse platea dictumst. Aenean vel augue lacinia, maximus massa eu, rutrum quam. Nulla facilisi. Sed aliquet, lorem vel euismod sodales, magna ipsum bibendum urna, vitae sagittis tortor sem vel leo. Maecenas et neque vel leo blandit condimentum. Phasellus ante metus, laoreet non mattis at, pharetra vel purus.Praesent blandit ex vitae leo facilisis mattis. Etiam quam ipsum, imperdiet nec tempor vehicula, lacinia quis velit. In hac habitasse platea dictumst. Sed consectetur purus tortor, ac ullamcorper sem ullamcorper eu. Proin in volutpat ligula. Donec ac purus mattis, iaculis metus quis, ultrices odio. Pellentesque dapibus, mi sed iaculis eleifend, odio nibh blandit leo, imperdiet tincidunt tellus mauris a tellus. Curabitur diam nibh, cursus et euismod ac, volutpat ut ipsum. Vivamus dignissim mollis turpis vel lobortis. </div>",
+                                    "$items": []
+                                }
+                            ]
+                        },
+                        {
+                            "$type": "block",
+                            "$name": "two",
+                            "$title": {
+                                "value": "Two"
+                            },
+                            "$items": [
+                                {
+                                    "$type": "html",
+                                    "$html": "<div>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec mi a sem fermentum iaculis sit amet eget odio. Duis consectetur sollicitudin arcu eu convallis. In hac habitasse platea dictumst. Aenean vel augue lacinia, maximus massa eu, rutrum quam. Nulla facilisi. Sed aliquet, lorem vel euismod sodales, magna ipsum bibendum urna, vitae sagittis tortor sem vel leo. Maecenas et neque vel leo blandit condimentum. Phasellus ante metus, laoreet non mattis at, pharetra vel purus.Praesent blandit ex vitae leo facilisis mattis. Etiam quam ipsum, imperdiet nec tempor vehicula, lacinia quis velit. In hac habitasse platea dictumst. Sed consectetur purus tortor, ac ullamcorper sem ullamcorper eu. Proin in volutpat ligula. Donec ac purus mattis, iaculis metus quis, ultrices odio. Pellentesque dapibus, mi sed iaculis eleifend, odio nibh blandit leo, imperdiet tincidunt tellus mauris a tellus. Curabitur diam nibh, cursus et euismod ac, volutpat ut ipsum. Vivamus dignissim mollis turpis vel lobortis. </div>",
+                                    "$items": []
+                                }
+                            ]
+                        },
+                        {
+                            "$type": "block",
+                            "$name": "three",
+                            "$title": {
+                                "value": "Three"
+                            },
+                            "$items": [
+                                {
+                                    "$type": "html",
+                                    "$html": "<div>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec mi a sem fermentum iaculis sit amet eget odio. Duis consectetur sollicitudin arcu eu convallis. In hac habitasse platea dictumst. Aenean vel augue lacinia, maximus massa eu, rutrum quam. Nulla facilisi. Sed aliquet, lorem vel euismod sodales, magna ipsum bibendum urna, vitae sagittis tortor sem vel leo. Maecenas et neque vel leo blandit condimentum. Phasellus ante metus, laoreet non mattis at, pharetra vel purus.Praesent blandit ex vitae leo facilisis mattis. Etiam quam ipsum, imperdiet nec tempor vehicula, lacinia quis velit. In hac habitasse platea dictumst. Sed consectetur purus tortor, ac ullamcorper sem ullamcorper eu. Proin in volutpat ligula. Donec ac purus mattis, iaculis metus quis, ultrices odio. Pellentesque dapibus, mi sed iaculis eleifend, odio nibh blandit leo, imperdiet tincidunt tellus mauris a tellus. Curabitur diam nibh, cursus et euismod ac, volutpat ut ipsum. Vivamus dignissim mollis turpis vel lobortis. </div>",
+                                    "$items": []
+                                }
+                            ]
+                        },
+                        {
+                            "$type": "block",
+                            "$name": "four",
+                            "$title": {
+                                "value": "Four"
+                            },
+                            "$items": [
+                                {
+                                    "$type": "html",
+                                    "$html": "<div>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec mi a sem fermentum iaculis sit amet eget odio. Duis consectetur sollicitudin arcu eu convallis. In hac habitasse platea dictumst. Aenean vel augue lacinia, maximus massa eu, rutrum quam. Nulla facilisi. Sed aliquet, lorem vel euismod sodales, magna ipsum bibendum urna, vitae sagittis tortor sem vel leo. Maecenas et neque vel leo blandit condimentum. Phasellus ante metus, laoreet non mattis at, pharetra vel purus.Praesent blandit ex vitae leo facilisis mattis. Etiam quam ipsum, imperdiet nec tempor vehicula, lacinia quis velit. In hac habitasse platea dictumst. Sed consectetur purus tortor, ac ullamcorper sem ullamcorper eu. Proin in volutpat ligula. Donec ac purus mattis, iaculis metus quis, ultrices odio. Pellentesque dapibus, mi sed iaculis eleifend, odio nibh blandit leo, imperdiet tincidunt tellus mauris a tellus. Curabitur diam nibh, cursus et euismod ac, volutpat ut ipsum. Vivamus dignissim mollis turpis vel lobortis. </div>",
+                                    "$items": []
+                                }
+                            ]
+                        }
+                        ,
+                        {
+                            "$type": "block",
+                            "$name": "five",
+                            "$title": {
+                                "value": "Five"
+                            },
+                            "$items": [
+                                {
+                                    "$type": "html",
+                                    "$html": "<div>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec mi a sem fermentum iaculis sit amet eget odio. Duis consectetur sollicitudin arcu eu convallis. In hac habitasse platea dictumst. Aenean vel augue lacinia, maximus massa eu, rutrum quam. Nulla facilisi. Sed aliquet, lorem vel euismod sodales, magna ipsum bibendum urna, vitae sagittis tortor sem vel leo. Maecenas et neque vel leo blandit condimentum. Phasellus ante metus, laoreet non mattis at, pharetra vel purus.Praesent blandit ex vitae leo facilisis mattis. Etiam quam ipsum, imperdiet nec tempor vehicula, lacinia quis velit. In hac habitasse platea dictumst. Sed consectetur purus tortor, ac ullamcorper sem ullamcorper eu. Proin in volutpat ligula. Donec ac purus mattis, iaculis metus quis, ultrices odio. Pellentesque dapibus, mi sed iaculis eleifend, odio nibh blandit leo, imperdiet tincidunt tellus mauris a tellus. Curabitur diam nibh, cursus et euismod ac, volutpat ut ipsum. Vivamus dignissim mollis turpis vel lobortis. </div>",
+                                    "$items": []
+                                }
+                            ]
+                        },
+                        {
+                            "$type": "block",
+                            "$name": "six",
+                            "$title": {
+                                "value": "Six"
+                            },
+                            "$items": [
+                                {
+                                    "$type": "html",
+                                    "$html": "<div>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec mi a sem fermentum iaculis sit amet eget odio. Duis consectetur sollicitudin arcu eu convallis. In hac habitasse platea dictumst. Aenean vel augue lacinia, maximus massa eu, rutrum quam. Nulla facilisi. Sed aliquet, lorem vel euismod sodales, magna ipsum bibendum urna, vitae sagittis tortor sem vel leo. Maecenas et neque vel leo blandit condimentum. Phasellus ante metus, laoreet non mattis at, pharetra vel purus.Praesent blandit ex vitae leo facilisis mattis. Etiam quam ipsum, imperdiet nec tempor vehicula, lacinia quis velit. In hac habitasse platea dictumst. Sed consectetur purus tortor, ac ullamcorper sem ullamcorper eu. Proin in volutpat ligula. Donec ac purus mattis, iaculis metus quis, ultrices odio. Pellentesque dapibus, mi sed iaculis eleifend, odio nibh blandit leo, imperdiet tincidunt tellus mauris a tellus. Curabitur diam nibh, cursus et euismod ac, volutpat ut ipsum. Vivamus dignissim mollis turpis vel lobortis. </div>",
+                                    "$items": []
+                                }
+                            ]
+                        },
+                        {
+                            "$type": "block",
+                            "$name": "seven",
+                            "$title": {
+                                "value": "Seven"
+                            },
+                            "$items": [
+                                {
+                                    "$type": "html",
+                                    "$html": "<div>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec mi a sem fermentum iaculis sit amet eget odio. Duis consectetur sollicitudin arcu eu convallis. In hac habitasse platea dictumst. Aenean vel augue lacinia, maximus massa eu, rutrum quam. Nulla facilisi. Sed aliquet, lorem vel euismod sodales, magna ipsum bibendum urna, vitae sagittis tortor sem vel leo. Maecenas et neque vel leo blandit condimentum. Phasellus ante metus, laoreet non mattis at, pharetra vel purus.Praesent blandit ex vitae leo facilisis mattis. Etiam quam ipsum, imperdiet nec tempor vehicula, lacinia quis velit. In hac habitasse platea dictumst. Sed consectetur purus tortor, ac ullamcorper sem ullamcorper eu. Proin in volutpat ligula. Donec ac purus mattis, iaculis metus quis, ultrices odio. Pellentesque dapibus, mi sed iaculis eleifend, odio nibh blandit leo, imperdiet tincidunt tellus mauris a tellus. Curabitur diam nibh, cursus et euismod ac, volutpat ut ipsum. Vivamus dignissim mollis turpis vel lobortis. </div>",
+                                    "$items": []
+                                }
+                            ]
+                        },
+                        {
+                            "$type": "block",
+                            "$name": "eight",
+                            "$title": {
+                                "value": "Eight"
+                            },
+                            "$items": [
+                                {
+                                    "$type": "html",
+                                    "$html": "<div>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec mi a sem fermentum iaculis sit amet eget odio. Duis consectetur sollicitudin arcu eu convallis. In hac habitasse platea dictumst. Aenean vel augue lacinia, maximus massa eu, rutrum quam. Nulla facilisi. Sed aliquet, lorem vel euismod sodales, magna ipsum bibendum urna, vitae sagittis tortor sem vel leo. Maecenas et neque vel leo blandit condimentum. Phasellus ante metus, laoreet non mattis at, pharetra vel purus.Praesent blandit ex vitae leo facilisis mattis. Etiam quam ipsum, imperdiet nec tempor vehicula, lacinia quis velit. In hac habitasse platea dictumst. Sed consectetur purus tortor, ac ullamcorper sem ullamcorper eu. Proin in volutpat ligula. Donec ac purus mattis, iaculis metus quis, ultrices odio. Pellentesque dapibus, mi sed iaculis eleifend, odio nibh blandit leo, imperdiet tincidunt tellus mauris a tellus. Curabitur diam nibh, cursus et euismod ac, volutpat ut ipsum. Vivamus dignissim mollis turpis vel lobortis. </div>",
+                                    "$items": []
+                                }
+                            ]
+                        }]
+                },
+                {
+                    $customColSize: 'demo-minw-200 hidden-xs-down hidden-xs',
+                    $items: [{
+                        $type: 'block',
+                        $style: "demo-fixed",
+                        $items: []
+                    }]
+                }
+            ]
+        }
+        ]
+    };
+    var schema = {
+        "type": "object",
+        "properties": {
+            "content": {
+                "type": "string"
+            }
+
+        },
+        "links": {
+        }
+
+
+    };
+
+
+    var data = { content: '' };
+
+    (function om() {
+        var lu = Phoenix.layoutUtils;
+        var names = lu.namesOfLayout(layout);
+        var ii = layout.$items[0].$items[1].$items[0];
+        names.forEach(item => {
+            schema.links[item.name] = { title: item.title };
+            ii.$items.push({ $bind: '$links.' + item.name });
+        });
+        Phoenix.ui.OpenForm(
+            $('#form'), layout, schema, data, {},
+            function (action, data) {
+                if (action.operation === 'execute') {
+                    this.scrollTo(action.property.substr('$links.'.length));
+                }
+            });
+    })();
+});
+
